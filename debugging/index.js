7930adcdb2a7e2fdcacfa627870282e0ec51b136
Poornima3787-developer <taehyungpurple02@gmail.com>
2025-04-12 16:13:13 +0530

    postman_express

--- debugging/index.js
+++ debugging/index.js
@@ -2,30 +2,21 @@ const express=require('express');
 const app=express();
 const PORT=3000;
 //Authentication middleware
-app.use("/welcome",(req,res,next)=>{
- console.log("Welcome");
- next();
+app.use(express.json());
+
+let users=[
+  {id:1,name:'Alice'},
+  {id:2,name:'Bob'}
+];
+
+app.get("/users",(req,res)=>{
+  res.json(users);
 })
-app.use((req,res,next)=>{
-  console.log("Authentication middleware");
-  next();
-})
-app.use("/library-2",(req,res,next)=>{
-  console.log("book recommendations");
-  next();
-})
-app.use("/library-3",(req,res,next)=>{
-  console.log("Special access to research paper from professors and seniors ");
-  next();
-})
-app.get("/welcome",(req,res)=>{
-  res.send("<h1>Welcome</h1>")
-})
-app.get("/library-2",(req,res)=>{
-  res.send("<h1>Library 2 Entered</h1>")
-})
-app.get("/library-3",(req,res)=>{
-  res.send("<h1>Library 3 is entered</h1>");
+app.post("/users",(req,res)=>{
+  const {name}=req.body;
+  const newUser={id:users.length+1,name};
+  users.push(newUser);
+  res.status(201).json(newUser);
 })
 app.listen(PORT,()=>{
   console.log("Server is running")
