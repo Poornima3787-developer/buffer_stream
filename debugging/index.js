f4771be9ce6803c193f016f5e843471e02e852ab
Poornima3787-developer <taehyungpurple02@gmail.com>
2025-04-12 15:01:40 +0530

    custome_middleware

--- debugging/index.js
+++ debugging/index.js
@@ -1,11 +1,32 @@
-//const http=require('http');
 const express=require('express');
 const app=express();
 const PORT=3000;
-app.use((req,res,next)=>{
-  console.log(`Server is up and running on port ${PORT}! Ready to handle requests.`);
-  res.send("<h1>Hello</h1>")
+//Authentication middleware
+app.use("/welcome",(req,res,next)=>{
+ console.log("Welcome");
+ next();
 })
-/*const server=http.createServer(app)
-server.listen(3000);*/
-app.listen(PORT);
+app.use((req,res,next)=>{
+  console.log("Authentication middleware");
+  next();
+})
+app.use("/library-2",(req,res,next)=>{
+  console.log("book recommendations");
+  next();
+})
+app.use("/library-3",(req,res,next)=>{
+  console.log("Special access to research paper from professors and seniors ");
+  next();
+})
+app.get("/welcome",(req,res)=>{
+  res.send("<h1>Welcome</h1>")
+})
+app.get("/library-2",(req,res)=>{
+  res.send("<h1>Library 2 Entered</h1>")
+})
+app.get("/library-3",(req,res)=>{
+  res.send("<h1>Library 3 is entered</h1>");
+})
+app.listen(PORT,()=>{
+  console.log("Server is running")
+});
