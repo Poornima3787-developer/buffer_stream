79ed8fd5ed8deb98f0b68d2738a71b3b6ea99aa9
Poornima3787-developer <taehyungpurple02@gmail.com>
2025-04-13 12:36:32 +0530

    routes

--- debugging/practise_express.js
+++ debugging/practise_express.js
@@ -1,18 +1,22 @@
 const express=require('express');
 const app=express();
-const port =3000;
+const port =4000;
 app.use(express.json());
-app.get('/orders',(req,res)=>{
-  res.send('Here is the list of all orders.');
+app.get('/products',(req,res)=>{
+  res.send('Here is the list of all products..');
 })
-app.post('/orders',(req,res)=>{
-  res.send('A new order has been created.');
+app.post('/products',(req,res)=>{
+  res.send('A new product has been added.');
 })
-app.get('/users',(req,res)=>{
-  res.send('Here is the list of all users')
+app.get('/categories',(req,res)=>{
+  res.send('Here is the list of all categories.')
 })
-app.post('/users',(req,res)=>{
-  res.send('A new user has been added.')
+app.post('/categories',(req,res)=>{
+  res.send('A new category has been created.')
+})
+app.all('*',(req,res)=>{
+  res.status(404);
+  res.send('<h1>404-Page Not Found');
 })
 app.listen(port,()=>{
   console.log(`Server is running on http://localhost:${port}`);
